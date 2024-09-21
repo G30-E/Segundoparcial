59abe0a57f9866d52dd4cc941a17499b72117d13
G30-E <gfloriane@miumg.edu.gt>
2024-09-21 11:08:59 -0600

    el que dice numeros primos es el verdadero ejercicio d el que dice numeros primos me di cuenta que tiene un error, porfa perdoneme la vida ;(

---
+++ Numeros primos.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool esPrimo(int numero) {
+    if (numero <= 1) {
+        return false; 
+    }
+    for (int i = 2; i <= numero / 2; i++) {
+        if (numero % i == 0) {
+            return false; 
+        }
+    }
+    return true; 
+}
+
+int main() {
+    int numero;
+
+    
+    cout << "Escriba un número entero positivo: ";
+    cin >> numero;
+
+   
+    if (esPrimo(numero)) {
+        cout << numero << " Es número primo." << endl;
+    } else {
+        cout << numero << " No es número primo." << endl;
+    }
+
+    return 0;
+}
